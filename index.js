841c9cf51ac3a0f957dc3ffd4af5b88c2d816b77
kawsar9867 <kbosuniya@gmail.com>
2026-03-08 15:15:40 +0600

    Login function work done

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Login function
+function login(){
+  let user = document.getElementById("userID").value;
+  let pass = document.getElementById("password").value;
+
+  let correctUser = "admin"
+  let currectPass = "admin123"
+
+  if(user === correctUser && pass === currectPass){
+    window.location.href = "home.html";
+    alert("Login Successful")
+  }
+  else{
+    alert("User ID or Password Incorrect");
+  }
+}
+
 fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
   .then((res) => res.json())
   .then((data) => cardContainer(data.data));
